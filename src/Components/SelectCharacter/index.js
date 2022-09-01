003e9c9af2a2af384c3bf6ee9fcac21792ceaa75
Fabio Jun <fabiojundev@gmail.com>
2022-09-01 15:21:42 -0300

    Add render default surfers

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -1,10 +1,79 @@
 import React, { useEffect, useState } from "react";
 import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import mySurfGame from "../../utils/MySurfGame.json";
 
 const SelectCharacter = ({ setCharacterNFT }) => {
+    const [characters, setCharacters] = useState([]);
+    const [gameContract, setGameContract] = useState(null);
+
+    useEffect(() => {
+        const { ethereum } = window;
+
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum);
+            const signer = provider.getSigner();
+            const gameContract = new ethers.Contract(
+                CONTRACT_ADDRESS,
+                mySurfGame.abi,
+                signer
+            );
+
+            setGameContract(gameContract);
+        } else {
+            console.log("Objeto Ethereum não encontrado");
+        }
+    }, []);
+
+    useEffect(() => {
+        const getCharacters = async () => {
+            try {
+                console.log("Buscando contrato de personagens para mintar");
+
+                /*
+                 * Get all available surfers
+                 */
+                const charactersTxn = await gameContract.getAllDefaultCharacters();
+                console.log("charactersTxn:", charactersTxn);
+
+                const characters = charactersTxn.map((characterData) =>
+                    transformCharacterData(characterData)
+                );
+
+                setCharacters(characters);
+            } catch (error) {
+                console.error("Algo deu errado ao buscar personagens:", error);
+            }
+        };
+
+        if (gameContract) {
+            getCharacters();
+        }
+    }, [gameContract]);
+
+    const renderCharacters = () =>
+        characters.map((character, index) => (
+            <div className="character-item" key={character.name}>
+                <div className="name-container">
+                    <p>{character.name}</p>
+                </div>
+                <img src={character.imageURI} alt={character.name} />
+                <button
+                    type="button"
+                    className="character-mint-button"
+                // onClick={mintCharacterNFTAction(index)} 
+                // você deve descomentar essa linha depois que criar a função mintCharacterNFTAction
+                >{`Mintar ${character.name}`}</button>
+            </div>
+        ));
+
     return (
         <div className="select-character-container">
             <h2>Minte seu Surfista 🌊 🏄🏻 🏄‍♀️</h2>
+            {characters.length > 0 && (
+                <div className="character-grid">{renderCharacters()}</div>
+            )}
         </div>
     );
 };
